b8ddc811551d2313f38be5ee08797326034c4a20
Jelani Alexander <jelanialexander82@gmail.com>
2022-12-30 23:03:14 +0900

    added Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import theme from "./theme";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/Home/index";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <ThemeProvider theme={theme}>
+      <CssBaseLine />
+      <Router>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Router>
+    </ThemeProvider>
+  );
+};
 
-export default App
+export default App;
