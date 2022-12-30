a9ce6a7e1671a0e7a42e6bb0c744fb9ba311a9b0
Jelani Alexander <jelanialexander82@gmail.com>
2022-12-30 21:33:47 +0900

    added createTheme

--- theme.js
+++ theme.js
@@ -1,5 +1,9 @@
-import {createTheme} from '@material-ui/core';
+import { createTheme } from "@material-ui/core";
 
 export default createTheme({
-    
-})
+  palette: {
+    background: {
+      default: "#f1f3f4",
+    },
+  },
+});
