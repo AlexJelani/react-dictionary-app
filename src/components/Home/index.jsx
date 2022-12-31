5042daa5ed00f9b6d51096db0f524f29652bad50
Jelani Alexander <jelanialexander82@gmail.com>
2022-12-31 12:23:32 +0900

    Typography added need formating

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,16 +1,25 @@
-import { Typography, Box, FilledInput, IconButton} from '@mui/material'
-
-
-
+import { Typography, Box, FilledInput, IconButton } from "@mui/material";
+import { Search as SearchIcon, Bookmark as BookmarkIcon } from "@mui/icons-material";
 
 const Home = () => {
   return (
-    <Box>
-      <img src="/assets/book.png" alt="Book```" />
+    <Box sx={{
+      display:'flex',
+      flexDirection:'column',
+      alignItems:'center',
+      justifyContent:'center',
+      height:'100vh'
+    }}>
+      <img src="src/assets/book.png" alt="Book" />
+      <Typography variant="h4">Dictionary</Typography>
+      <Typography>Find the meaning quick</Typography>
+      <FilledInput/>
+      <IconButton>
+        <BookmarkIcon/>
+      </IconButton>
 
     </Box>
-  )
-}
+  );
+};
 
-export default Home
-  
+export default Home;
--- src/theme.js
+++ src/theme.js
@@ -2,9 +2,11 @@ import { createTheme } from "@mui/material/styles";
 
 export default createTheme({
   palette: {
-    primary: {
-      main: "#1760a5",
-      light: "skyblue",
+    background: {
+      default: "#F1F3F4"
     },
   },
+  // typography:{
+  //   fontFamily:"Mulish","san-serif"
+  // }
 });
