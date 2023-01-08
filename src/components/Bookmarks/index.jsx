e53d481dbd569a44ebf5582e2992446b4d50ca46
Jelani Alexander <jelanialexander82@gmail.com>
2023-01-08 09:03:37 +0900

    added bookmarks page

--- src/components/Bookmarks/index.jsx
+++ src/components/Bookmarks/index.jsx
@@ -5,14 +5,17 @@ import { Link } from "react-router-dom";
 const Bookmarks = ({ bookmarks }) => {
   return (
     <>
-      <Stack sx={{mb:2}} direction="row" alignItems="center">
-        <IconButton sx={{ color: "black", mr: 1 }}>
+      <Stack sx={{ mb: 2 }} direction="row" alignItems="center">
+        <IconButton to="/" component={Link} sx={{ color: "black", mr: 1 }}>
           <BackIcon />
         </IconButton>
         <Typography variant="h6">Bookmark</Typography>
       </Stack>
       {Object.keys(bookmarks).map((b) => (
-        <Box key={b} to={`/search/${b}`} component={Link}
+        <Box
+          key={b}
+          to={`/search/${b}`}
+          component={Link}
           sx={{
             p: 2,
             cursor: "pointer",
@@ -20,12 +23,13 @@ const Bookmarks = ({ bookmarks }) => {
             borderRadius: 1,
             textTransform: "capitalize",
             mb: 2,
-            fontweight:800,
-            display:'block',
-            color:'black',
-            textDecoration:'none
-            '
-          }}>
+            fontweight: 800,
+            display: "block",
+            color: "black",
+
+            textDecoration: "none",
+          }}
+        >
           {b}
         </Box>
       ))}
